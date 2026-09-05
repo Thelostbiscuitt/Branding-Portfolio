7fe1e0bd9f41c941671aa9d0d8bdc0f4418427aa
Thelostbiscuitt <randoranderson7@gmail.com>
2026-09-05 04:52:23 +0100

    Add a restrained loading screen: the logo with a travelling beam and preparing-the-showcase line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Syne, DM_Sans, DM_Mono } from 'next/font/google'
 import Cursor from '@/components/Cursor/Cursor'
+import Loading from '@/components/Loading/Loading'
 import './globals.css'
 
 const syne = Syne({
@@ -55,6 +56,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${syne.variable} ${dmSans.variable} ${dmMono.variable}`}>
       <body>
+        <Loading />
         <Cursor />
         {children}
       </body>
